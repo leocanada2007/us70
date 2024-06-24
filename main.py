03e7534d42ed1f672b1f9b16b99763493a41bd54
leocanada2007 <71792477+leocanada2007@users.noreply.github.com>
2024-06-24 10:00:24 -0700

    Update main.py

--- main.py
+++ main.py
@@ -209,8 +209,7 @@ def tab1():
     
     # fig_summary.update_layout(legend_title="Stats", bargap=0.5,bargroupgap=0.1)
 
-    # COLOR
-    # fig_summary.update_traces(change['color'])
+    fig_summary.update_traces(change['color'])
     
     # fig_summary.for_each_trace(
     #     lambda trace: trace.update(marker_color=np.where(change.loc[change['Stats'].eq(trace.name), 'Value'] < 0, 'red', 'green'))
