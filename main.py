40a72b62dda5cc1fab3d5f67fcbb247d11acb42b
leocanada2007 <71792477+leocanada2007@users.noreply.github.com>
2024-06-25 16:42:42 -0700

    Update main.py

--- main.py
+++ main.py
@@ -230,16 +230,27 @@ def tab1():
     plt.tick_params(axis='both', which='major', labelsize=16)
     
     with col10:
-        st.pyplot(fig=plt)    
+        st.pyplot(fig=plt)  
 
-    df1.plot(x = 'DATE', y = ['RSAFS_MoM'], title='Retail Sales, MoM')
-    plt.axvspan(start_date, end_date, color = 'red', alpha = 0.25)
-    plt.xticks(rotation=45)
-    plt.title("Retail Sales, MoM", fontsize = 24)
-    plt.tick_params(axis='both', which='major', labelsize=16)
-    
-    with col11:
-        st.pyplot(fig=plt)   
+    if start_year >= 1969 and start_year >= start_year <= 1987
+        df1.plot(x = 'DATE', y = ['WTI'], title='WTI Price')
+        plt.axvspan(start_date, end_date, color = 'red', alpha = 0.25)
+        plt.xticks(rotation=45)
+        plt.title("WTI Price", fontsize = 24)
+        plt.tick_params(axis='both', which='major', labelsize=16)
+
+        with col11:
+            st.pyplot(fig=plt)   
+            
+    else:
+        df1.plot(x = 'DATE', y = ['RSAFS_MoM'], title='Retail Sales, MoM')
+        plt.axvspan(start_date, end_date, color = 'red', alpha = 0.25)
+        plt.xticks(rotation=45)
+        plt.title("Retail Sales, MoM", fontsize = 24)
+        plt.tick_params(axis='both', which='major', labelsize=16)
+        
+        with col11:
+            st.pyplot(fig=plt)   
 
 
     
